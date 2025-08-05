fbb4744229104b1515e82ac45c70eb2176e18288
Motostore <mymotostorellc@gmail.com>
2025-08-05 00:43:08 -0400

    Fix type error in edit page for Vercel build

--- src/app/dashboard/products/[id]/edit/page.tsx
+++ src/app/dashboard/products/[id]/edit/page.tsx
@@ -1,13 +1,12 @@
 'use client'
 import Breadcrumbs from "@/app/ui/transactions/breadcrumbs";
 import HeaderProfile from "@/app/ui/dashboard/header-profile";
-import { Recharge, Streaming } from "@/app/lib/definitions";
+import { Streaming } from "@/app/lib/definitions";
 import { useEffect, useState } from "react";
 import { fetchProductById } from "@/app/lib/products.service";
 import Form from "../../form";
 
 export default function Page({ params }: { params: { id: string } }) {
-
   const link = "/dashboard/products/";
   const productPath = "streaming";
   const id = params.id;
@@ -44,16 +43,15 @@ export default function Page({ params }: { params: { id: string } }) {
             ]}
           />
         </div>
-          <div className="flex items-start md:items-end flex-col mt-4 md:mt-0 order-2 md:order-1 mb-4 md:mb-0">
-              <HeaderProfile />
-          </div>
+        <div className="flex items-start md:items-end flex-col mt-4 md:mt-0 order-2 md:order-1 mb-4 md:mb-0">
+          <HeaderProfile />
+        </div>
       </div>
       <hr className='w-full h-1 bg-gray-200 mx-auto my-5' />
       {
         item 
-        ?
-        <Form provider={item} />
-        :null
+        ? <Form provider={item} />
+        : null
       }
     </main>
   )
